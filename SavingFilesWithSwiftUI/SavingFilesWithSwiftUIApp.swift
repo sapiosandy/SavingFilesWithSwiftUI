3dfaf53f5afdc238ec2ea033838efb9850fe3184
Sandy Gomez <sgomez1029@gmail.com>
2024-04-16 14:47:07 -0700

    Initial Commit

---
+++ SavingFilesWithSwiftUI/SavingFilesWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingFilesWithSwiftUIApp.swift
+//  SavingFilesWithSwiftUI
+//
+//  Created by Sandra Gomez on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SavingFilesWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
